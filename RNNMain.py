e5b1cb5fa67cfdc4134efec49a5225f5af2eed7c
AubreyFeldker <brf190002@utdallas.edu>
2023-05-01 01:27:06 -0500

    reprocessed data, working on imporvements

--- RNNMain.py
+++ RNNMain.py
@@ -11,23 +11,35 @@ import datetime
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import normalize
+from RecurrentNeuralNetwork import RNN
 
 df = pd.read_csv('https://raw.githubusercontent.com/AubreyFeldker/CS4375TeamProject/main/processed_weather_df.csv')
 
+# Removes rows missing values in the one-hot
 df[df['Cold'].astype(bool)]  
 
-df = df.sort_values(by = 'Timestamp')
+df = df.sort_values(by = ['Latitude', 'Longitude', 'Timestamp'])
 df = df.reset_index()
 
-X = df.drop('Precipitation(in)', axis=1)
-Y = df['Precipitation(in)']
+print(df[['Latitude', 'Longitude']].value_counts())
 
 split_ratio = 0.8
 split_index = int(len(df) * split_ratio)
 
-X_train = X[:split_index]
-Y_train = Y[:split_index]
-X_test = X[split_index:]
-Y_test = Y[split_index:]
+df_groups = df.groupby(['Latitude', 'Longitude'])
 
-print(X_train)
+rnn = RNN(internal_layers=3, input_nodes=len(df.columns) - 1, output_nodes=7)
+
+# Applying the neural network across each set of lat/long, since they are constants during a recursive network
+for name, group in df_groups:
+    # 80/20 test-train split along each group
+    split_index = int(len(group) * split_ratio)
+    #rnn.train(training_data=group[:split_index], epochs=4, sequence_length=10)
+    group = group.drop(['Timestamp'], axis = 1)
+
+    # Normalization of timestamps within the group to change how it works to time elapsed for next item
+    for i in range (len(group) - 1):
+        group.iloc[i] = (group.iloc[i+1] - group.iloc[i]) / 1000
+
+    values, output = rnn.forward_pass(group.iloc[0])
+    rnn.backward_pass(values, output, np.array([1, 2, 3, 4, 5, 6, 7]))
--- RecurrentNeuralNetwork.py
+++ RecurrentNeuralNetwork.py
@@ -1,49 +1,67 @@
-import random, math
+import random, math, pandas as pd, numpy as np
 
-class RecurrentNeuralNetwork:
+class RNN:
 
-    def __init__(self, internal_layers, input_nodes, update_weight=.05):
+    def __init__(self, internal_layers, input_nodes, output_nodes, update_weight=.0005):
         self.num_layers = internal_layers + 2
         self.nodes_per_layer = input_nodes + 1 # bias nodes
+        self.output_nodes = output_nodes
         self.update_weight = update_weight
 
-        self.weight_matrix = []
+        # 3D matrix containing all the weights for the neural network, then one more for output later
+        self.weight_matrix = np.random.randn(internal_layers, self.nodes_per_layer, self.nodes_per_layer-1)
+        self.output_weight_matrix = np.random.randn(self.nodes_per_layer, output_nodes)
 
-        # Create value/weight matrix for a full 
-        for i in range(0, self.num_layers * self.nodes_per_layer):
-            node_weights = []
-            for j in range(0, self.nodes_per_layer):
-                node_weights.append(random.random())
-            self.weight_matrix.append((1,node_weights))
-
-    def sigmoid(self, x):
-        return 1 / (1 + math.exp(-x))
+    # tanh activation function utilized
+    def tanh(self, x):
+        return np.tanh(x)
+    def tanh_derivative(self, x):
+        return 1 - self.tanh(x) ** 2
     
-    def make_prediction(self, input_variables):
-        if (len(input_variables) != self.nodes_per_layer - 1):
-            print("Error: Improper number of input variables")
-            return
+    def forward_pass(self, input):
+        assert len(input) == self.nodes_per_layer - 1, "Error: Improper number of input variables"
+
+        # Append bias value to the node
+        layer_values = np.zeros((self.num_layers-2, self.nodes_per_layer))
+        layer_values[0] = np.append(input.to_numpy(), [1])
+
+        # Iterate through layers in NN, applying tanh at each node
+        for i in range(1, self.num_layers-2):
+            layer_values[i] = np.append(self.tanh(layer_values[i-1].dot(self.weight_matrix[i])), [1])
+
+        # Last iteration into the output nodes
+        output = self.tanh(layer_values[i].dot(self.output_weight_matrix))
+        return layer_values, output
+    
+    def backward_pass(self, layer_values, output, expected):
+        # Get the delta values of the output nodes via tanh_der * error
+        layer = self.num_layers - 2
+        delta_layer = self.tanh_derivative(output) * (expected - output)
+
+        # Calculating the weight changes for the weights to the output nodes
+        self.output_weight_matrix += (self.update_weight * output.dot(delta_layer))
+
+        delta_layer = self.tanh_derivative(layer_values[layer-1]) * self.output_weight_matrix.dot(delta_layer)
+
+        # Calculating the weight changes for the rest of the nodes
+        for layer in range(self.num_layers-3, 0, -1):
+            self.output_weight_matrix += (self.update_weight * layer_values[layer-1].dot(delta_layer))
+
+            delta_layer = self.tanh_derivative(layer_values[layer-1]) * self.weight_matrix[layer].dot(delta_layer[0:-1])
+
+        # Final weight update calculation for the weights connected to the input nodes
+        self.output_weight_matrix += (self.update_weight * layer_values[0].dot(delta_layer))
+
+
+    def test(self, test_items, sequence_length):
         
-        for i in range(0, self.nodes_per_layer-1):
-            self.weight_matrix[i] = (input_variables[i], self.weight_matrix[i][1])
+        # Iterate through each temportal sequence of [sequence length] times, performing forward & back propagation
+        for sequence_start in range(0, len(test_items) - sequence_length - 1):
+            loss = 0
+            input = test_items.iloc[i]
 
-        for layer in range(1, self.num_layers):
-            this_layer_start = self.nodes_per_layer * layer
-            prev_layer_start = self.nodes_per_layer * (layer-1)
+            for i in range(sequence_start, sequence_start + sequence_length):
+                layer_values, output = self.forward_pass(input)
 
-            for node in range(0, self.nodes_per_layer - 1):
-                total_sum = 0
-                for i in range(0, self.nodes_per_layer):
-                    total_sum += self.weight_matrix[prev_layer_start+i][0] * self.weight_matrix[prev_layer_start+i][1][this_layer_start+node]
-
-                self.weight_matrix[this_layer_start+node] = (self.sigmoid(total_sum), self.weight_matrix[this_layer_start+node][1])
-
-        # Get final prediction array from the final set of nodes
-        final_prediction = []
-
-        for i in range((self.num_layers-1) * self.nodes_per_layer, len(self.weight_matrix)-1):
-            final_prediction.append(self.weight_matrix[i][0])
-
-        return final_prediction
 
 
